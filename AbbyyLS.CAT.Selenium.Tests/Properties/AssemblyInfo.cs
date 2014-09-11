df6aa19f6df1c06365c1c741959e4ee6c0b41bb0
Irina Kaidalova <i.kaidalova@abbyy-ls.com>
2014-09-11 18:02:48 +0400

    PRX-4816 Рефакторинг Selenium-тестов

--- AbbyyLS.CAT.Selenium.Tests/Properties/AssemblyInfo.cs
+++ AbbyyLS.CAT.Selenium.Tests/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("AbbyyLS.CAT.LingvoPro.Selenium.Tests")]
+[assembly: AssemblyTitle("AbbyyLS.CAT.Selenium.Tests")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("AbbyyLS.CAT.LingvoPro.Selenium.Tests")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("AbbyyLS.CAT.Selenium.Tests")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e83874de-e2a8-422a-9558-4497178af986")]
+[assembly: Guid("03ccbcb1-2fde-409d-af3c-c4151d409fce")]
 
 // Version information for an assembly consists of the following four values:
 //
--- Scripts/upload_file.au3
+++
@@ -1,8 +0,0 @@
-; ждем пока окно загрузки будет активным
-Local $handle = WinWaitActive("[CLASS:#32770]", "");
-; убеждаемся что окно активно
-WinFlash($handle);
-
-ControlSetText($handle, "", "Edit1", $CmdLine[1]);
-
-Send("{ENTER}");
