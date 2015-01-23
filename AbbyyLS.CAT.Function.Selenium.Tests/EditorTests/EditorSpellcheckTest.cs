0b52114f3ba7d44dc701765d034a8049f9023429
p.perevozkin@abbyy-ls.com <p.perevozkin@abbyy-ls.com>
2015-01-23 19:47:19 +0600

    PRX-7419 Починить тесты на спеллчек (в этом коммите только прописал нужный namespace)

--- AbbyyLS.CAT.Function.Selenium.Tests/EditorTests/EditorSpellcheckTest.cs
+++ AbbyyLS.CAT.Function.Selenium.Tests/EditorTests/EditorSpellcheckTest.cs
@@ -2,7 +2,7 @@
 using NUnit.Framework;
 using System.Collections.Generic;
 
-namespace AbbyyLS.CAT.Function.Selenium.Tests
+namespace AbbyyLS.CAT.Function.Selenium.Tests.Editor
 {
 	/// <summary>
 	/// Группа тестов для проверки словаря орфографии в редакторе
