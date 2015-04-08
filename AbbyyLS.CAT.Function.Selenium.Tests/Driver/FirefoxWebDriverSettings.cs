0b3266e3ebb0e34b2f9444faa8c3c077f7f10a8c
Denis Mozhegov <d.mozhegov@abbyy-ls.com>
2015-04-08 17:59:40 +0300

    Корректировка настроек FF

--- AbbyyLS.CAT.Function.Selenium.Tests/Driver/FirefoxWebDriverSettings.cs
+++ AbbyyLS.CAT.Function.Selenium.Tests/Driver/FirefoxWebDriverSettings.cs
@@ -15,11 +15,10 @@ namespace AbbyyLS.CAT.Function.Selenium.Tests.Driver
 			var profile = new FirefoxProfile { AcceptUntrustedCertificates = true };
 			profile.SetPreference("browser.download.dir", PathProvider.ResultsFolderPath);
 			profile.SetPreference("browser.download.folderList", 2);
-			profile.SetPreference("browser.download.useDownloadDir", false);
-			//profile.SetPreference("browser.download.manager.showWhenStarting", false);
+			profile.SetPreference("browser.download.useDownloadDir", true);
 			profile.SetPreference("browser.helperApps.alwaysAsk.force", false);
 			profile.SetPreference
-				("browser.helperApps.neverAsk.saveToDisk", "text/xml, text/csv, text/plain, text/log, application/zip, application/x-gzip, application/x-compressed, application/vnd.openxmlformats-officedocument.wordprocessingml.document, application/msword, application/octet-stream");
+				("browser.helperApps.neverAsk.saveToDisk", "application/xml, text/xml, text/csv, text/plain, text/log, application/zip, application/x-gzip, application/x-compressed, application/x-gtar, multipart/x-gzip, application/tgz, application/gnutar, application/x-tar, application/x-xliff+xml,  application/msword.docx, application/pdf, application/x-pdf, application/octetstream, application/x-ttx, application/x-tmx, application/octet-stream, application/vnd.openxmlformats-officedocument.wordprocessingml.document, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet");
 			Driver = new FirefoxDriver(profile);
 			ProcessNames = new[] { "firefox", "conhost" };
 		}
