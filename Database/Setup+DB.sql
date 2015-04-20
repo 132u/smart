21783d48fb28c8e5f8cbd3928eda1d9da5c40d0b
Anastasia Korol <a.korol@abbyy-ls.com>
2015-04-20 15:47:57 +0300

    Убрала из скрипт sql указание базы

--- Database/Setup+DB.sql
+++ Database/Setup+DB.sql
@@ -1,6 +1,2 @@
-USE [SmartCat_AutoTests]
-SET QUOTED_IDENTIFIER ON
-SET XACT_ABORT ON
-
 INSERT INTO Auth.Users (ID, NickName, EMail, PreferredLanguageID, IsAdmin)
 VALUES ('97CA8F26-431F-4CEE-BF9E-AB019C5E24C7', 'Bobby Test', 'bobby@mailforspam.com', 25, 1)
