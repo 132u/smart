ea8c7407e0fdb4e964443aeb78f8afc4d3d90d36
p.perevozkin@abbyy-ls.com <p.perevozkin@abbyy-ls.com>
2015-05-18 12:54:21 +0600

    SCAT-222 Починить новые тесты

--- AbbyyLS.SmartCAT.Selenium.Tests/Pages/Workspace/WorkspacePage.cs
+++ AbbyyLS.SmartCAT.Selenium.Tests/Pages/Workspace/WorkspacePage.cs
@@ -246,7 +246,7 @@ namespace AbbyyLS.SmartCAT.Selenium.Tests.Pages.Workspace
 		protected IWebElement ProjectsButton { get; set; }
 
 		protected const string CAT_MENU = "//div[contains(@class, 'js-mainmenu')]";
-		protected const string CAT_MENU_OPEN_BUTTON = "//h2[@class='g-topbox__header']/a";
+		protected const string CAT_MENU_OPEN_BUTTON = "//h2[contains(@class,'g-topbox__header')]/a";
 		protected const string CLOSE_HELP_BUTTON = "//div[@class='hopscotch-bubble animated']//button[contains(@class,'hopscotch-cta')]";
 
 		protected const string RESOURCES_MENU ="//ul[contains(@class, 'serviceMenu')]//li[contains(@class, 'js-menuitem-Resources')]";
