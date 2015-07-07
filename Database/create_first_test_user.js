fd677a6d69a1785abd1a47acfab18672afbf0da3
p.perevozkin@abbyy-ls.com <p.perevozkin@abbyy-ls.com>
2015-07-07 13:18:10 +0600

    SCAT-255 Поменять скрипт создания первого пользователя

---
+++ Database/create_first_test_user.js
@@ -0,0 +1,12 @@
+db.Auth.Users.insert({ 
+    "_id" : BinData(3, "Yop29JSggUCbrxaXzCDLsA=="), 
+    "email" : "teamcity@mailforspam.com", 
+    "nickName" : "Teamcity Test", 
+    "isAdmin" : true, 
+    "interfaceLanguageId" : NumberInt(9),
+    "hasPlainPassword" : true, 
+    "oAuthCredentialTypes" : NumberInt(0),  
+    "dateCreated" : ISODate("2015-06-17T05:35:31.097+0000"), 
+    "preferredSourceLanguageId" : NumberInt(9), 
+    "preferredTargetLanguageId" : NumberInt(25)
+});
