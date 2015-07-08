5593ca1b4470138ad0c058d0f0a28db4dd41ccb0
Irina Khaptanova <i.khaptanova@abbyy-ls.com>
2015-07-08 11:40:24 +0300

    SCAT-335 фикс тестов на глоссарий

--- AbbyyLS.SmartCAT.Selenium.Tests/Pages/Glossaries/GlossariesPage.cs
+++ AbbyyLS.SmartCAT.Selenium.Tests/Pages/Glossaries/GlossariesPage.cs
@@ -63,6 +63,12 @@ namespace AbbyyLS.SmartCAT.Selenium.Tests.Pages.Glossaries
 			Logger.Debug("Нажать кнопку создания глоссария.");
 			CreateGlossaryButton.JavaScriptClick();
 
+			if (!Driver.WaitUntilElementIsDisplay(By.XPath(SAVE_GLOSSARY_BUTTON)))
+			{
+				Logger.Trace("Повторный клик по кнопке создания глоссария.");
+				CreateGlossaryButton.JavaScriptClick();
+			}
+
 			return new NewGlossaryDialog().GetPage();
 		}
 
@@ -87,7 +93,7 @@ namespace AbbyyLS.SmartCAT.Selenium.Tests.Pages.Glossaries
 		public DateTime GlossaryDateModified(string glossaryName)
 		{
 			Logger.Trace("Получить дату изменения глоссария {0}.", glossaryName);
-
+			
 			return DateTime.ParseExact(Driver.SetDynamicValue(How.XPath, MODIFIED_DATE, glossaryName).Text,
 				"M/d/yyyy h:mm tt",CultureInfo.InvariantCulture);
 		}
@@ -112,7 +118,9 @@ namespace AbbyyLS.SmartCAT.Selenium.Tests.Pages.Glossaries
 		protected const string CREATE_GLOSSARY_BUTTON = ".//span[contains(@class,'js-create-glossary-button')]//a";
 		protected const string GLOSSARY_TABLE = "//table[contains(@class,'js-sortable-table') and contains(@data-sort-action, 'Glossaries')]";
 		protected const string GLOSSARY_ROW = "//tr[contains(@class, 'js-glossary-row')]/td[1]/p[text()='*#*']";
-		protected const string MODIFIED_DATE = "//tr[contains(@class, 'js-glossary-row')]/td[1]/p[text() = '*#*']/../../td[8]";
-		protected const string AUTHOR = "//tr[contains(@class, 'js-glossary-row')]/td[1]/p[text() = '*#*']/../../td[9]/p";
+		protected const string MODIFIED_DATE = "//tr[contains(@class, 'js-glossary-row')]/td[1]/p[text() = '*#*']/../../td[last()]/preceding::td[1]";
+		protected const string AUTHOR = "//tr[contains(@class, 'js-glossary-row')]/td[1]/p[text() = '*#*']/../../td[last()]/p";
+
+		protected const string SAVE_GLOSSARY_BUTTON = ".//div[contains(@class,'js-popup-edit-glossary')][2]//span[@data-bind='click: save']";
 	}
 }
