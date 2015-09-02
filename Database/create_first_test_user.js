15ef4c931433f38152b6a68d4f825d31e8d5ba72
Irina Khaptanova <i.khaptanova@abbyy-ls.com>
2015-09-02 15:57:15 +0300

    SCAt-431 добавила новых пользователей в скрипт для параллельного прогона

--- Database/create_first_test_user.js
+++ Database/create_first_test_user.js
@@ -10,3 +10,55 @@ db.Auth.Users.save({
     "preferredSourceLanguageId" : NumberInt(9), 
     "preferredTargetLanguageId" : NumberInt(25)
 });
+
+db.Auth.Users.save({ 
+    "_id" : BinData(3, "gPQCc8XCEk27HlYJk3qZ5w=="), 
+    "email" : "teamcity_thread1@mailforspam.com", 
+    "nickName" : "Teamcity Thread1", 
+    "isAdmin" : true, 
+    "interfaceLanguageId" : NumberInt(9),
+    "hasPlainPassword" : true, 
+    "oAuthCredentialTypes" : NumberInt(0),  
+    "dateCreated" : ISODate("2015-09-02T12:26:22.166+0000"),
+    "preferredSourceLanguageId" : NumberInt(9), 
+    "preferredTargetLanguageId" : NumberInt(25)
+});
+
+db.Auth.Users.save({ 
+    "_id" : BinData(3, "8V9i7/X9nEWOldTx+L4xvA=="), 
+    "email" : "teamcity_thread2@mailforspam.com", 
+    "nickName" : "Teamcity Thread2", 
+    "isAdmin" : true, 
+    "interfaceLanguageId" : NumberInt(9),
+    "hasPlainPassword" : true, 
+    "oAuthCredentialTypes" : NumberInt(0),  
+    "dateCreated" : ISODate("2015-09-02T12:51:00.576+0000"),
+    "preferredSourceLanguageId" : NumberInt(9), 
+    "preferredTargetLanguageId" : NumberInt(25)
+});
+
+db.Auth.Users.save({ 
+    "_id" : BinData(3, "B71eWROOekOggwXQSD1isw=="), 
+    "email" : "teamcity_thread3@mailforspam.com", 
+    "nickName" : "Teamcity Thread3", 
+    "isAdmin" : true, 
+    "interfaceLanguageId" : NumberInt(9),
+    "hasPlainPassword" : true, 
+    "oAuthCredentialTypes" : NumberInt(0),  
+    "dateCreated" : ISODate("2015-09-02T12:34:32.501+0000"),
+    "preferredSourceLanguageId" : NumberInt(9), 
+    "preferredTargetLanguageId" : NumberInt(25)
+});
+
+db.Auth.Users.save({ 
+    "_id" : BinData(3, "w2ww8vak9UG/fKYwAQXQIw=="), 
+    "email" : "teamcity_thread4@mailforspam.com", 
+    "nickName" : "Teamcity Thread4", 
+    "isAdmin" : true, 
+    "interfaceLanguageId" : NumberInt(9),
+    "hasPlainPassword" : true, 
+    "oAuthCredentialTypes" : NumberInt(0),  
+    "dateCreated" : ISODate("2015-09-02T12:53:09.085+0000")
+    "preferredSourceLanguageId" : NumberInt(9), 
+    "preferredTargetLanguageId" : NumberInt(25)
+});
