73368d0511a0800d8b95e4a4d4077f86fabf759f
Irina Khaptanova <i.khaptanova@abbyy-ls.com>
2015-09-08 16:54:41 +0300

    SCAT-454 дополнительный фикс тестов на экспорт

--- AbbyyLS.SmartCAT.Selenium.Tests/Pages/Projects/DocumentSettings.cs
+++ AbbyyLS.SmartCAT.Selenium.Tests/Pages/Projects/DocumentSettings.cs
@@ -106,7 +106,7 @@ namespace AbbyyLS.SmartCAT.Selenium.Tests.Pages.Projects
 		protected  IWebElement GlossaryCheckbox { get; set; }
 
 		protected const string TITLE = "(//h2[text()='Document Settings'])[3]";
-		protected const string NAME_INPUT = "//div[contains(@class,'js-popup-document-settings')][2]//input[contains(@data-bind,'value: name')]";
+		protected const string NAME_INPUT = "//div[contains(@class,'document-settings')][3]//input[contains(@data-bind,'value: name')]";
 		protected const string SAVE_BUTTON = "//div[contains(@class,'g-popup-bd js-popup-bd js-popup-single-target-document-settings')][2]//span[contains(@data-bind,'click: save')]";
 		protected const string MT_CHECKBOX = "//div[@class='g-popup-bd js-popup-bd js-popup-single-target-document-settings'][2]//tbody[contains(@data-bind, 'machineTranslators')]//p[text()='*#*']/parent::td/..//td//input";
 		protected const string GLOSSARY_BY_NAME_XPATH = "(//h2[text()='Document Settings']//..//..//table[contains(@class,'l-corpr__tbl')]//tbody[@data-bind='foreach: glossaries']//tr[contains(string(), '*#*')])[1]//td//input";
