17e79dd61b449750cd0c6f2279476b24874534e9
Anastasia Korol <a.korol@abbyy-ls.com>
2015-09-10 18:33:03 +0300

    SCAT-460 Отключила тесты на форматы

--- AbbyyLS.SmartCAT.Selenium.Tests/Tests/Projects/DocumentFormatTests.cs
+++ AbbyyLS.SmartCAT.Selenium.Tests/Tests/Projects/DocumentFormatTests.cs
@@ -11,6 +11,7 @@ namespace AbbyyLS.SmartCAT.Selenium.Tests.Tests.Projects
 	[Standalone]
 	class DocumentFormatTests<TWebDriverProvider> : BaseTest<TWebDriverProvider> where TWebDriverProvider : IWebDriverProvider, new()
 	{
+		[Explicit("SCAT-460")]
 		[TestCase("docFile.doc")]
 		[TestCase("docxFile.docx")]
 		[TestCase("htmFile.htm")]
