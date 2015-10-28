e279ff9ecea4c7ef5418e7b5e0760e122d2ea8e6
Anastasia Korol <a.korol@abbyy-ls.com>
2015-10-28 17:55:05 +0300

    SCAT-358 Добавила категории и параллельность

--- AbbyyLS.SmartCAT.Selenium.Tests/Tests/Glossaries/GlossaryEditStructureTermFieldsTest.cs
+++ AbbyyLS.SmartCAT.Selenium.Tests/Tests/Glossaries/GlossaryEditStructureTermFieldsTest.cs
@@ -1,13 +1,14 @@
-﻿using AbbyyLS.SmartCAT.Selenium.Tests.Pages.Glossaries;
-
-using NUnit.Framework;
-
+﻿using NUnit.Framework;
 using AbbyyLS.SmartCAT.Selenium.Tests.DataStructures;
 using AbbyyLS.SmartCAT.Selenium.Tests.Drivers;
+using AbbyyLS.SmartCAT.Selenium.Tests.FeatureAttributes;
+using AbbyyLS.SmartCAT.Selenium.Tests.Pages.Glossaries;
 using AbbyyLS.SmartCAT.Selenium.Tests.TestHelpers;
 
 namespace AbbyyLS.SmartCAT.Selenium.Tests.Tests.Glossaries
 {
+	[Standalone]
+	[Parallelizable(ParallelScope.Fixtures)]
 	public class GlossaryEditStructureTermFieldsTest<TWebDriverProvider> : BaseTest<TWebDriverProvider> where TWebDriverProvider : IWebDriverProvider, new()
 	{
 		[SetUp]
