2504affde08007e20fa5154a9ae8493e5989eb37
Anastasia Korol <a.korol@abbyy-ls.com>
2015-11-05 10:00:28 +0300

    SCAT-573 включила тест UnderlineWord (PRX-13058)

--- AbbyyLS.SmartCAT.Selenium.Tests/Tests/Editor/EditorSpellcheckTests.cs
+++ AbbyyLS.SmartCAT.Selenium.Tests/Tests/Editor/EditorSpellcheckTests.cs
@@ -89,7 +89,7 @@ namespace AbbyyLS.SmartCAT.Selenium.Tests.Tests.Editor
 		}
 
 		[TestCase("Планета")]
-		[TestCase("Чуть-чуть"), Ignore("PRX-13058")]
+		[TestCase("Чуть-чуть")]
 		public void UnderlineWord(string word)
 		{
 			var wrongWord = string.Format("Ы{0}", word);
