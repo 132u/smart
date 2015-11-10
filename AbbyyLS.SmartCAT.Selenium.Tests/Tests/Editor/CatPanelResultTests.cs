a41b21ce36d0413fd2ee3a7ce366ccc0fd8ba66b
Anastasia Korol <a.korol@abbyy-ls.com>
2015-11-10 13:06:52 +0300

    Добавила пропущенные параллельные атрибуты

--- AbbyyLS.SmartCAT.Selenium.Tests/Tests/Editor/CatPanelResultTests.cs
+++ AbbyyLS.SmartCAT.Selenium.Tests/Tests/Editor/CatPanelResultTests.cs
@@ -11,6 +11,7 @@ using AbbyyLS.SmartCAT.Selenium.Tests.TestHelpers;
 
 namespace AbbyyLS.SmartCAT.Selenium.Tests.Tests.Editor.CatPanelTests
 {
+	[Parallelizable(ParallelScope.Fixtures)]
 	public class CatPanelResultsTest<TWebDriverProvider> : BaseTest<TWebDriverProvider> where TWebDriverProvider : IWebDriverProvider, new()
 	{
 		[SetUp]
--- AbbyyLS.SmartCAT.Selenium.Tests/Tests/Glossaries/GlossaryTermFilterTests.cs
+++ AbbyyLS.SmartCAT.Selenium.Tests/Tests/Glossaries/GlossaryTermFilterTests.cs
@@ -13,6 +13,7 @@ using AbbyyLS.SmartCAT.Selenium.Tests.TestFramework;
 
 namespace AbbyyLS.SmartCAT.Selenium.Tests.Tests.Glossaries
 {
+	[Parallelizable(ParallelScope.Fixtures)]
 	public class GlossaryTermFilterTests<TWebDriverProvider> : BaseTest<TWebDriverProvider> where TWebDriverProvider : IWebDriverProvider, new()
 	{
 		[SetUp]
