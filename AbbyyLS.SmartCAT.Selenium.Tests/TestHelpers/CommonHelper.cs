04bd7def6f3602e10ce7445cb0047d77d9bc0bff
Irina Khaptanova <i.khaptanova@abbyy-ls.com>
2015-12-11 12:05:41 +0300

    SCAT-697 Добавить логирование переходов по прямым ссылкам

--- AbbyyLS.SmartCAT.Selenium.Tests/TestHelpers/CommonHelper.cs
+++ AbbyyLS.SmartCAT.Selenium.Tests/TestHelpers/CommonHelper.cs
@@ -14,28 +14,53 @@ namespace AbbyyLS.SmartCAT.Selenium.Tests.TestHelpers
 			Driver = driver;
 		}
 
+		/// <summary>
+		/// Переход на страницу авторизации sign-in.
+		/// </summary>
 		public void GoToSignInPage()
 		{
+			CustomTestContext.WriteLine("Переход на страницу авторизации: {0}.", ConfigurationManager.Url + RelativeUrlProvider.SignIn);
+
 			Driver.Navigate().GoToUrl(ConfigurationManager.Url + RelativeUrlProvider.SignIn);
 		}
 
+		/// <summary>
+		/// Переход на страницу регистрации компаний /corp-reg.
+		/// </summary>
 		public void GoToCompanyRegistration()
 		{
+			CustomTestContext.WriteLine("Переход на страницу регистрации компаний: {0}.", ConfigurationManager.Url + RelativeUrlProvider.CorpReg);
+
 			Driver.Navigate().GoToUrl(ConfigurationManager.Url + RelativeUrlProvider.CorpReg);
 		}
 
+		/// <summary>
+		/// Переход на страницу регистрации фрилансеров /freelance-reg.
+		/// </summary>
 		public void GoToFreelanceRegistratioin()
 		{
+			CustomTestContext.WriteLine("Переход на страницу регистрации фрилансеров: {0}.", ConfigurationManager.Url + RelativeUrlProvider.FreelanceRegistratioin);
+
 			Driver.Navigate().GoToUrl(ConfigurationManager.Url + RelativeUrlProvider.FreelanceRegistratioin);
 		}
 
+		/// <summary>
+		/// Переход на страницу администрирования.
+		/// </summary>
 		public void GoToAdminUrl()
 		{
+			CustomTestContext.WriteLine("Переход на страницу администрирования: {0}", ConfigurationManager.AdminUrl);
+
 			Driver.Navigate().GoToUrl(ConfigurationManager.AdminUrl);
 		}
 
+		/// <summary>
+		/// Переход на страницу Workspace.
+		/// </summary>
 		public void GoToWorkspaceUrl(string workspaceUrl)
 		{
+			CustomTestContext.WriteLine("Переход на страницу Workspace: {0}.", workspaceUrl);
+
 			Driver.Navigate().GoToUrl(workspaceUrl);
 		}
 	}
