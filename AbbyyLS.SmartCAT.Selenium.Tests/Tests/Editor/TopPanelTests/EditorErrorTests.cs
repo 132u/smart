1ff7080c47d00755add76bcb02ce073d1ce3e5f6
Dmitriy Tonkushin <d.tonkushin@abbyy-ls.com>
2016-03-16 12:41:27 +0500

    SCAT-908 Починить тест FindErrorButtonTest и FindErrorHotkeyTest

--- AbbyyLS.SmartCAT.Selenium.Tests/Tests/Editor/TopPanelTests/EditorErrorTests.cs
+++ AbbyyLS.SmartCAT.Selenium.Tests/Tests/Editor/TopPanelTests/EditorErrorTests.cs
@@ -1,19 +1,52 @@
-﻿using NUnit.Framework;
+﻿using System.IO;
+
+using NUnit.Framework;
 
 using AbbyyLS.SmartCAT.Selenium.Tests.Drivers;
 using AbbyyLS.SmartCAT.Selenium.Tests.FeatureAttributes;
 using AbbyyLS.SmartCAT.Selenium.Tests.Pages.Editor;
+using AbbyyLS.SmartCAT.Selenium.Tests.Pages.Login;
+using AbbyyLS.SmartCAT.Selenium.Tests.Pages.Projects;
+using AbbyyLS.SmartCAT.Selenium.Tests.Pages.Projects.ProjectSettings;
+using AbbyyLS.SmartCAT.Selenium.Tests.Pages.Projects.ProjectSettings.SettingsDialog;
+using AbbyyLS.SmartCAT.Selenium.Tests.Pages.Workspace;
+using AbbyyLS.SmartCAT.Selenium.Tests.TestHelpers;
 
 namespace AbbyyLS.SmartCAT.Selenium.Tests.Tests.Editor
 {
 	[Parallelizable(ParallelScope.Fixtures)]
 	[Standalone]
-	public class EditorErrorTests<TWebDriverProvider> : EditorBaseTest<TWebDriverProvider> where TWebDriverProvider : IWebDriverProvider, new()
+	public class EditorErrorTests<TWebDriverProvider>
+		: BaseTest<TWebDriverProvider> where TWebDriverProvider : IWebDriverProvider, new()
 	{
 		[SetUp]
 		public void Initialization()
 		{
 			_errorsDialog = new ErrorsDialog(Driver);
+			_createProjectHelper = new CreateProjectHelper(Driver);
+			_projectsPage = new ProjectsPage(Driver);
+			_projectSettingsPage = new ProjectSettingsPage(Driver);
+			_projectSettingsHelper = new ProjectSettingsHelper(Driver);
+			_editorPage = new EditorPage(Driver);
+			_selectTaskDialog = new SelectTaskDialog(Driver);
+			_projectsPage = new ProjectsPage(Driver);
+
+			var projectUniqueName = _createProjectHelper.GetProjectUniqueName();
+
+			_createProjectHelper.CreateNewProject(
+				projectUniqueName,
+				filePath: PathProvider.EditorTxtFile,
+				glossaryName: GlossariesHelper.UniqueGlossaryName());
+
+			_projectsPage.ClickProject(projectUniqueName);
+
+			_projectSettingsHelper
+				.AssignTasksOnDocument(PathProvider.EditorTxtFile, ThreadUser.NickName);
+
+			_projectSettingsPage.OpenDocumentInEditorWithTaskSelect(
+				Path.GetFileNameWithoutExtension(PathProvider.EditorTxtFile));
+
+			_selectTaskDialog.SelectTask();
 		}
 
 		[Test(Description = "Проверяет открытие диалога поиска ошибок с помощью кнопки")]
@@ -35,5 +68,11 @@ namespace AbbyyLS.SmartCAT.Selenium.Tests.Tests.Editor
 		}
 
 		private ErrorsDialog _errorsDialog;
+		private CreateProjectHelper _createProjectHelper;
+		private ProjectSettingsHelper _projectSettingsHelper;
+		private ProjectSettingsPage _projectSettingsPage;
+		private EditorPage _editorPage;
+		private SelectTaskDialog _selectTaskDialog;
+		private ProjectsPage _projectsPage;
 	}
 }
